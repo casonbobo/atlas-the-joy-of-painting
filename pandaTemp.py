1fb70cb6a742e4b8b572c6e0bf4635c4904a894b
casonbobo <casonsbobo@gmail.com>
2024-03-30 13:32:58 -0500

    The data should be finished. Now I just need to connect the data to the sever

--- pandaTemp.py
+++ pandaTemp.py
@@ -5,10 +5,12 @@ with open('temp.csv', 'r') as input_file:
     reader = csv.reader(input_file)
     data = list(reader)
 
-# Replace the first column with iterating count starting at 1 and prefixed with 'EID'
-for row_index in range(1, len(data)):
-    # Replace the first column with 'EID' followed by the row index plus 1
-    data[row_index][0] = f'EID{row_index + 1}'
+# Append 'COL' to the front of each index of an array in the second column
+for row_index in range(len(data)):
+    # Assuming the second column is the one you want to modify
+    # Adjust the index if your CSV structure is different
+    second_column_index = 1 # Indexing starts at 0, so 1 is the second column
+    data[row_index][second_column_index] = 'COL' + data[row_index][second_column_index]
 
 # Open the output CSV file
 with open('temp.csv', 'w', newline='') as output_file:
